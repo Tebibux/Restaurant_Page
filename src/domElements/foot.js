005fc7d5ea68de0c09e393be60fc5cc1801512aa
tebibux <tebibusolomon79@gmail.com>
2022-11-20 17:34:13 +0300

    finished

--- src/domElements/foot.js
+++ src/domElements/foot.js
@@ -1,7 +1,9 @@
 const footer = () => {
+	const gitLink = 'https://github.com/Tebibux/Restaurant_Page'
 	const foot = document.createElement('div');
 	foot.className = 'footer';
-	foot.innerText = 'Biniam Fasts';
+	foot.innerHTML = `TOP - <a href="http://github.com/Tebibux/calculator" target="_blank"><i
+	class="fab fa-github" aria-hidden="true"></i>Tebibux</a>`;
 	return foot;
 }
 
--- src/links/Menu.js
+++ src/links/Menu.js
@@ -4,6 +4,11 @@ import Link from '../domElements/container';
 import footer from '../domElements/foot';
 import  navigation from '../domElements/nav';
 import FoodContent from '../domElements/foodContent';
+import img1 from '../asset/image/food_1.png';
+import img2 from '../asset/image/food_2.png';
+import img3 from '../asset/image/food_3.png';
+import img4 from '../asset/image/food_4.png';
+import img5 from '../asset/image/food_5.png';
 
 // loading the container
 const container = document.querySelector('.container');
@@ -28,28 +33,48 @@ const Menu = () => {
 	const main = mainMenu.elementDom();
 
 	// creating the menu items 1
-	const food_One_ = new FoodContent('', 'Special Ful', `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
-	dolorem commodi nostrum optio quam neque libero. Dolorum vel unde magnam earum exercitationem
-	voluptatibus, dignissimos, voluptatem iusto in expedita, velit blanditiis.`);
+	const imgOne = new Image();
+	imgOne.src = img1;
+	const food_One_ = new FoodContent(`${imgOne.src}`, 'Doro Wot', `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus.`);
 	const foodOne = food_One_.food();
 	main.appendChild(foodOne)
 
 
 	// creating the menu items 2
-	const food_Two_ = new FoodContent('', 'Special Ful', `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
-	dolorem commodi nostrum optio quam neque libero. Dolorum vel unde magnam earum exercitationem
-	voluptatibus, dignissimos, voluptatem iusto in expedita, velit blanditiis.`);
+	const imgTwo = new Image();
+	imgTwo.src = img2;
+	const food_Two_ = new FoodContent(`${imgTwo.src}`, 'Beye Aynetu', `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus.`);
 	const foodTwo = food_Two_.food();
 	main.appendChild(foodTwo);
 
 
 
 	// creating the menu items 3
-	const food_Three_ = new FoodContent('', 'Special Ful', `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
-	dolorem commodi nostrum optio quam neque libero. Dolorum vel unde magnam earum exercitationem
-	voluptatibus, dignissimos, voluptatem iusto in expedita, velit blanditiis.`);
+	const imgThree = new Image();
+	imgThree.src = img3;
+	const food_Three_ = new FoodContent(`${imgThree.src}`, 'Bini Special', `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus.`);
 	const foodThree = food_Three_.food();
+
 	main.appendChild(foodThree)
+
+	// creating the menu items 4
+	const imgFour = new Image();
+	imgFour.src = img4;
+	const food_Four_ = new FoodContent(`${ imgFour.src}`, 'Beyaynet Special', `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus.`);
+	const foodFour = food_Four_.food();
+
+
+	main.appendChild(foodFour)	
+
+	// creating the menu items 5
+	const imgFive = new Image();
+	imgFive.src = img5;
+	const food_five_ = new FoodContent(`${imgFive.src}`, 'Kitifo Special', `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus.`);
+	const foodFive = food_five_.food();
+
+
+	main.appendChild(foodFive)	
+
 	// appending the element
 	container.appendChild(main);
 	// _____________-- body menu edit --_________________
